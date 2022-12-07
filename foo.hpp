373baf221c7f300c52cc3e4f5204c011c1b745d2
Szymon <symonbielenin@gmail.com>
2022-12-07 18:27:52 +0100

    1st

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include "Human.hpp"
-
+#include <algorithm>
+#include <iostream>
 #include <list>
 #include <vector>
-
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > v1;
+    for (std::list< Human >::reverse_iterator p = people.rbegin(); p != people.rend(); ++p) {
+        p->birthday();
+        if (p->isMonster()) {
+            v1.push_back('n');
+        }
+        else {
+            v1.push_back('y');
+        }
+    }
+
+    return v1;
 }
